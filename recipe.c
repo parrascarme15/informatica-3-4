61052bea87c67c253604286ebe4406214a03a4d6
parrascarme15 <victorparraaranda@gmail.com>
2026-04-22 15:05:08 +0000

    vending machine

--- recipe.c
+++ recipe.c
@@ -3,4 +3,14 @@
 int main(void){
     printf("Coconut Cranberry Dessert\n");
     printf("\n");
+    printf("1 1/2 \t\t cups graham cracker crumbs\n");
+    printf("1/2 \t\t cup butter or margarine, melted\n");
+    printf("1 can \t\t sweetened condensed milk\n");
+    printf("1 cup \t\t flaked coconut\n");
+    printf("1 cup \t\t dried cranberries\n");
+    printf("2 cups \t\t vanilla chips\n");
+    printf("1 cup \t\t sliced almonds or pecans\n");
+    printf("\n");
+    printf("1. Combine graham cracker crumbs and butter. Press into a greased 13″ x 9″ pan.\n");
+    printf("2. Combine remaining ingredients and spread over graham cracker mixture. Bake at 350°F for 25 minutes or until the edges are golden brown. Let cool and cut into squares.\n");
 }
