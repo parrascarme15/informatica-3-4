9deac69bea06047e7f0349170b41eee0c49b0e65
parrascarme15 <victorparraaranda@gmail.com>
2026-04-22 15:05:24 +0000

    vending machine

---
+++ naming-variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// I made the #include to start
+int main(void){
+int number;
+// I fixed the name of this variable by deleting. You cant use signs for variables.
+int test_value;
+// The rule says that there must not be a space in between. I fixed it by using a guion bajo.
+int example;
+//I took off the 1
+float int_value;
+//I fixed the int by adding another name. "value"
+printf("Variables created successfully.\n");
+}
+
---
+++ vending.c
@@ -0,0 +1,32 @@
+#include <stdio.io>
+
+int main(void){
+    //Variables for TORTAS CHUMA
+    float price1 = 74.99;
+    int quantity1 = 15;
+    char select1 = 't';
+
+    //Variables for Coca Cola
+    float price2 = 24.99;
+    int quantity2 = 10;
+    char select2 = 'c';
+
+    //Variables for Tacos El Piporro
+    float price3 = 79.99;
+    int quantity3 = 30;
+    char select3 'p'
+
+    printf("Vending Machine Stock\n");
+    printf("-----------------------------\n");
+    printf("price1\n");
+    printf("quantity1\n");
+    printf("select1\n");
+    printf("\n");
+    printf("price2\n");
+    printf("quantity2\n");
+    printf("select2\n");
+    printf("\n");
+    printf("price3\n");
+    printf("quantity3\n");
+    printf("select3\n");
+}
