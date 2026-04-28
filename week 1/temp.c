2bb0615d427db9511e351f586b550e257f41d12f
parrascarme15 <victorparraaranda@gmail.com>
2026-04-28 14:56:58 +0000

    temp

--- week 1/temp.c
+++ week 1/temp.c
@@ -6,5 +6,5 @@ int main(void){
     scanf("%f", &Fahrenheit);
     printf("\n");
     Celsius = (Fahrenheit-32) / 1.8;
-    printf("Temperature in Celsius = %.2f \n", Celsius);
+    printf("Temperature in Celsius = %.2f \n", Celsius); // la misma temperatura de f y c es -40
 }
