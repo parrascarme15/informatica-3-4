6d179a48205803bc42078170b3faf3ad9fe62488
parrascarme15 <victorparraaranda@gmail.com>
2026-04-29 14:57:36 +0000

    test

---
+++ test/receipt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    char item;
+    printf("Enter name of the item: ");
+    scanf("%c",&item);
+    printf("\n");
+    int price;
+    printf("Enter price of the item: ");
+    scanf("%d",price);
+    printf("\n");
+    float quantity;
+    printf ("Enter quantity: ");
+    scanf("%f",quantity);
+    printf("\n");
+    printf("Alsuper\n");
+    printf("Thank you for your purchase\n");
+}
