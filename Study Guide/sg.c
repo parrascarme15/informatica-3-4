c7292f53649204c3c1b27e22edd77892bfc818ee
parrascarme15 <victorparraaranda@gmail.com>
2026-05-21 17:04:10 +0000

    study guide

---
+++ Study Guide/sg.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void){
+    float Celsius, Fahrenheit;
+    printf("Enter Temperature C: \n");
+    scanf("%f", &Celsius);
+    printf("\n");
+    Fahrenheit = (Celsius * 1.8) + 32;
+    printf("Temperature in Fahrenheit = %.2f \n", Fahrenheit);
+
+if(Celsius < 0){
+        printf("%.2f is a ❄️ Freezing weather. \n", Celsius);
+    } else if(Celsius < 10){
+        printf("%.2f is a 🥶 Very cold weather. \n", Celsius);
+    } else if(Celsius < 20){
+        printf("%.2f is a 🧥 Chilly weather. \n", Celsius);
+    } else if(Celsius < 30){
+        printf("%.2f is a 🖼️ Normal weather. \n", Celsius);
+    } else if(Celsius < 40){
+        printf("%.2f is a ☀️ Hot weather. \n", Celsius);
+    } else {
+        printf("%.2f is a 🔥 Very hot weather. \n", Celsius);
+    }
+}
+ //A layer mask is a non-destructive tool used to hide or reveal specific portions of a layer without permanently erasing the original data.
+ //Layers in design are like stacked transparent sheets. They allow you to separate elements, allowing you to edit, move, or hide individual components without affecting the rest of your artwork.
